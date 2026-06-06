8294cc2925128674c3aa52f678d0bf23dd119fe2
yefday090 <yefday090@gmail.com>
2026-06-06 18:43:35 -0500

    fix: update agenda API URL to HTTPS and add to CORS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   apiUrl: 'http://localhost:5047',
-  agendaApiUrl: 'http://localhost:5019',
-  recaptchaSiteKey: '6LfI7xAtAAAAABVEpIjsObWRYBBqYZeSxt3ZsCWX',
+  agendaApiUrl: 'https://localhost:7022',
+  recaptchaSiteKey: '6LfI7xAtAAAAAIYleerWZ-rJudoqe-IPMgfB_1b3',
 };
